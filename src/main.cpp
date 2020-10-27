bc2ef9527b7b7531fc10eb98e6f67ffb87a70834
Keven <kkurz@ctl.de>
2020-10-27 14:21:09 +0100

    Webserver and PROGMEM

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 #include <Arduino.h>
-#include "ArduinoJson.h"
-#include <string>
 #include "Music.cpp"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-// Network SSID
-const char *ssid = "AP-CTL-GAST";
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char *ssid = "AP-CTL";
 const char *password = "";
-const String user = "kkurz";
-const String token = "";
+
+ESP8266WebServer server(80);
+
+void handleNotFound()
+{
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
 
 void setup()
 {
@@ -29,61 +46,43 @@ void setup()
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
   Music::playCoin();
-}
 
-int lastBuild = 0;
-
-int jenkinsAPI(const String URL)
-{
-  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
-
-  client->setInsecure();
-
-  HTTPClient https;
-
-  if (https.begin(*client, URL))
+  
+  const char playURL[] = "/play/";
+  for (int i = 0; i < Music::COUNT; i++)
   {
-    int httpCode = https.GET();
-
-    if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
-    {
-      const size_t capacity = JSON_OBJECT_SIZE(2) + 70;
-      DynamicJsonDocument doc(capacity);
-
-      DeserializationError error = deserializeJson(doc, https.getString());
-      https.end();
-      if (error)
-      {
-        Serial.println("deserializeJson() failed: ");
-        Serial.println(error.c_str());
-      }
-      //https://arduinojson.org/v6/assistant/
-
-      return (int)doc["number"];
-    }
+    
+    const char *const title = Music::getTitle(i);
+    char uri[30];
+    strcpy(uri, playURL);
+    strcat(uri, title);
+    Serial.println(uri);
+    server.on(uri, [i]() {
+      server.send(200, "text/plain", "this works as well");
+      Music::play(i);
+    });
   }
-  else
-  {
-    Serial.printf("[HTTPS] Unable to connect\n");
-  }
-  return -1;
+
+  server.on("/", []() {
+    server.send(200, "text/plain", "this works as well");
+    Music::playRandom();
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop()
 {
+  server.handleClient();
+
   if (WiFi.isConnected())
   {
-
-    const int completed = jenkinsAPI("https://" + user + ":" + token + "@jenkins.ctl.de/job/LV/lastCompletedBuild/api/json?tree=number");
-    const int stable = jenkinsAPI("https://" + user + ":" + token + "@jenkins.ctl.de/job/LV/lastStableBuild/api/json?tree=number");
-    Serial.print(completed);
-    Serial.print(", ");
-    Serial.println(stable);
-    if ((completed != stable || completed < 0)) // lastBuild != completed && 
-      Music::playRandom();
-
-    lastBuild = completed;
+    server.handleClient();
+    //Music::play("1-UP");
   }
-
-  delay(60000);
+  /*
+  delay(10000);*/
 }
